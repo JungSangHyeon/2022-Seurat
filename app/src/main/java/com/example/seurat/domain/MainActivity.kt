f15a3874f47dc6761745bb69031ab2899419543c
jeongsanghyeon <sh199919@gmail.com>
2022-09-14 00:03:43 +0900

    Add pickImage Button

--- app/src/main/java/com/example/seurat/domain/MainActivity.kt
+++ app/src/main/java/com/example/seurat/domain/MainActivity.kt
@@ -6,15 +6,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import androidx.core.graphics.get
 import androidx.lifecycle.lifecycleScope
+import com.example.seurat.R
 import com.example.seurat.tech.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -31,6 +39,55 @@ class MainActivity : ComponentActivity() {
         private const val ballSize = 10
     }
 
+    private var canvasSize: IntSize? = null
+    private var pickImageBitmap: Bitmap? = null
+    private var pointageBitmap = mutableStateOf<Bitmap?>(null)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startDrawPointage()
+
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Canvas(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .onGloballyPositioned {
+                            canvasSize = it.size
+                        }
+                ){
+                    pointageBitmap.value?.let {
+                        drawImage(
+                            image = it.asImageBitmap(),
+                            topLeft = Offset(
+                                center.x - it.width/2,
+                                center.y - it.height/2
+                            )
+                        )
+                    }
+                }
+
+                Image(
+                    painter = painterResource(id = R.drawable.ic_baseline_brightness_1_24),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clickable { pickImage() }
+                )
+            }
+        }
+    }
+
+    private fun pickImage() {
+        launcher.launch(ContentPickType.Image)
+        pickImageBitmap = null
+        pointageBitmap.value = null
+    }
+
     private val launcher = ContentPick.createLauncher(this){ imageUri ->
         lifecycleScope.launch {
             pickImageBitmap = withContext(Dispatchers.IO) {
@@ -40,38 +97,6 @@ class MainActivity : ComponentActivity() {
                 } ?: throw Exception("Canvas Size Not Initialized") // never happen
 
                 adjustedBitmap
-            }
-        }
-    }
-
-    private var canvasSize: IntSize? = null
-    private var pickImageBitmap: Bitmap? = null
-    private var pointageBitmap = mutableStateOf<Bitmap?>(null)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        launcher.launch(ContentPickType.Image)
-
-        startDrawPointage()
-
-        setContent {
-            Canvas(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .onGloballyPositioned {
-                        canvasSize = it.size
-                    }
-            ){
-                pointageBitmap.value?.let {
-                    drawImage(
-                        image = it.asImageBitmap(),
-                        topLeft = Offset(
-                            center.x - it.width/2,
-                            center.y - it.height/2
-                        )
-                    )
-                }
             }
         }
     }
